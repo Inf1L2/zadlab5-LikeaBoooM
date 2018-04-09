46180e19027fc6aff458affc01ed98beb91e7c44
Mateusz Borkowski <37511105+LikeaBoooM@users.noreply.github.com>
2018-04-09 10:19:27 +0200

    Add files via upload

---
+++ OOPL5/Firma.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.edu.ur.oopl5;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LikeaBoooM
+ */
+
+public class Firma {
+    
+    Scanner wej = new Scanner(System.in);
+    Pracownik pracownicy[];
+    
+    
+    
+}
